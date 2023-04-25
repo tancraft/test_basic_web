dc9240dce02ec127ba3bb001263da2f78bd514fe
tancraft <tancraftwork.dev@gmail.com>
2023-04-26 01:39:55 +0200

    test 2

--- autoload.php
+++ autoload.php
@@ -2,11 +2,18 @@
 
 spl_autoload_register(function ($class_name) {
     $path = str_replace('\\', '/', $class_name);
-    $file = __DIR__ . '/App/' . $path . '.php';
+    $file = __DIR__ . '/' . $path . '.php';
 
     if (file_exists($file)) {
         require_once($file);
+    } else {
+        $file = __DIR__ . '/App/' . $path . '.php';
+
+        if (file_exists($file)) {
+            require_once($file);
+        }
     }
 });
+
 
 
