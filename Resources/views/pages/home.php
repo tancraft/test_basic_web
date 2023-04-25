c0a1361a7bddbf08f5d7ec7a531a7afbefde85ff
tancraft <tancraftwork.dev@gmail.com>
2023-04-26 01:43:40 +0200

    delete

--- Resources/views/pages/home.php
+++
@@ -1,3 +0,0 @@
-<h1>Bienvenue sur mon site web !</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id augue euismod, varius risus non, molestie justo. Sed at nibh vel lorem dictum lacinia ac quis lectus.</p>
-
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../autoload.php';
-
-use App\Router;
-
-$router = new Router();
-$router->dispatch();
